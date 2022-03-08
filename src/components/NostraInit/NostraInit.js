aa205a1f3811fe236a52b93de83fd79248888828
Shivkumar Patel <shivnpatel27@gmail.com>
2022-03-08 12:50:02 -0500

    adding code for db

--- src/components/NostraInit/NostraInit.js
+++ src/components/NostraInit/NostraInit.js
@@ -1,21 +1,21 @@
 import Async from 'react-async';
-// import db from '../db';
+import db from '../../db';
 
 const getData = async () => {
     const res = await fetch('https://prod-api.nostra.ai/content/?site=https://ec2.nostraprod.com&content=content&page=/&referrer=default', {
-        headers: {'x-api-key': '8OIjtrzO4o6rr4vglTZAx1hAbOJGzzQX5OMuwtMj'}
+        headers: { 'x-api-key': '8OIjtrzO4o6rr4vglTZAx1hAbOJGzzQX5OMuwtMj' }
     });
-    if(!res.ok) throw new Error(res.statusText);
+    if (!res.ok) throw new Error(res.statusText);
     return res.json();
 }
 
 export const NostraInit = () => {
     return <Async promiseFn={getData}>
         {({ data, error, isPending }) => {
-        if (isPending) return null
-        if (error) return null
-        if (data)
-            console.log(data)
+            if (isPending) return null
+            if (error) return null
+            if (data)
+                console.log(data)
             return null
         }}
     </Async>
--- src/stories/NostraText.stories.js
+++ src/stories/NostraText.stories.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {storiesOf} from '@storybook/react';
+import { storiesOf } from '@storybook/react';
 
-import {NostraText} from '../components/NostraText'
+import { NostraText } from '../components/NostraText'
 
 const stories = storiesOf('App Test', module);
 
 stories.add('App', () => {
     return (
-        <NostraText nostraTag="test" original="This is example text for Nostra" tag="h2" attributes={{"class" : "sample"}}/>
+        <NostraText nostraTag="test" original="This is example text for Nostra" tag="h2" attributes={{ "className": "sample" }} />
     );
 })
