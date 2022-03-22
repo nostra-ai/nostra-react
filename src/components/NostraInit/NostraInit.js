e8519d508d659623edfd51f99be5cf1d2d1101ff
Shiv Patel <shiv@nostra.ai>
2022-03-22 13:04:59 -0400

    adding code

--- src/components/NostraInit/NostraInit.js
+++ src/components/NostraInit/NostraInit.js
@@ -9,25 +9,55 @@ const getData = async () => {
     return res.json();
 }
 
-export const NostraInit = () => {
-    return <Async promiseFn={getData}>
-        {({ data, error, isPending }) => {
-            if (isPending) {
-                return null
-            }
-            if (error) {
-                return null
-            }
-            if (data) {
-                var db = new Dexie("nostra");
-                db.version(1).stores({
-                    localData: "id,data"
-                });
+const getProfile = async () => {
+    const res = await fetch('https://prod-api.nostra.ai/internal/?site=https://ec2.nostraprod.com&setting=config&page=/', {
+        headers: { 'x-api-key': '8OIjtrzO4o6rr4vglTZAx1hAbOJGzzQX5OMuwtMj' }
+    });
+    if (!res.ok) throw new Error(res.statusText);
+    return res.json();
+}
 
-                db.localData.add({ id: "content", data: JSON.stringify(data) });
-            }
-            return null
-        }}
-    </Async>
+export const NostraInit = () => {
+    return <>    
+        <Async promiseFn={getData}>
+            {({ data, error, isPending }) => {
+                if (isPending) {
+                    return null
+                }
+                if (error) {
+                    return null
+                }
+                if (data) {
+                    var db = new Dexie("nostra");
+                    db.version(1).stores({
+                        localData: "id,data"
+                    });
+
+                    db.localData.add({ id: "content", data: JSON.stringify(data) });
+                }
+                return null
+            }}
+        </Async>
+
+        <Async promiseFn={getProfile}>
+            {({ data, error, isPending }) => {
+                if (isPending) {
+                    return null
+                }
+                if (error) {
+                    return null
+                }
+                if (data) {
+                    var db = new Dexie("nostra");
+                    db.version(1).stores({
+                        localData: "id,data"
+                    });
+
+                    db.localData.add({ id: "profile", data: JSON.stringify(data) });
+                }
+                return null
+            }}
+        </Async>
+    </>
 
 }
