bd832b1d72754431ed1c72188d55853a7288247c
Shiv Patel <shiv@nostra.ai>
2022-03-30 19:00:03 -0400

    adding code

--- build/NostraInit.js
+++ build/NostraInit.js
@@ -329,11 +329,11 @@ export var NostraInit = function NostraInit() {
                     return null;
                 }
                 if (data) {
-                    // db_version.stores({
-                    //     localData: "id,data"
-                    // });
+                    db_version.stores({
+                        localData: "id,data"
+                    });
 
-                    // db.localData.add({ id: "content", data: JSON.stringify(data) });
+                    db.localData.add({ id: "content", data: JSON.stringify(data) });
 
                     console.log("content");
                     console.log(data);
